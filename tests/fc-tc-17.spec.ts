1b58eb63656564a5a6f330a8ce62a7e699a74779
navyaqsg <navya.kp@qsgsoft.com>
2026-06-08 15:23:11 +0530

    deleted old pages and tests

--- tests/fc-tc-17.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from '@playwright/test';
-
-test.describe('FC-TC-17 - Dairies search smoke', () => {
-  test('@new FC-TC-17 Verify search functionality on the Dairies page works end-to-end after abort signal implementation', async ({ page }) => {
-    await page.goto('/');
-  });
-});
--- tests/fc-tc-29.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from '@playwright/test';
-
-test.describe('FC-TC-29 - ART sender flow', () => {
-  test('@new test2 Foundation dairy herdcode shows warning and disables NEXT', async ({ page }) => {
-    await page.goto('/dairy/34858');
-  });
-});
